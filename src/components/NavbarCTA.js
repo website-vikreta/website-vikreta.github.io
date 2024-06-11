cef6692e678399274cf271fc490162add992cdb3
Samruddhi Bhabad <samu24106@gmail.com>
2024-06-11 21:41:24 +0530

    BUG:Fix help us fix notification

--- src/components/NavbarCTA.js
+++ src/components/NavbarCTA.js
@@ -1,33 +1,37 @@
-// importing react packages
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 const NavbarCTA = () => {
-   const [isNavbarCTAVisible, setIsNavbarCTAVisible] = useState(true);
+  const [isNavbarCTAVisible, setIsNavbarCTAVisible] = useState(true);
 
-   return (
-      <>
-         <div className="navbar-cta">
-            <div className="container">
-               <div className="text">
-                  Just a friendly reminder. Your brand will die without a good website. ☠️
-               </div>
-               <Link to="/contact" className="link" onClick={() => setIsNavbarCTAVisible(false)}>
-                  <span className="link-text">
-                     Help us fix
-                  </span>
-                  <span className="link-icon">
-                     <i className="bi bi-check-circle-fill"></i>
-                  </span>
-               </Link>
-               <div className={`close ${isNavbarCTAVisible ? 'show' : 'hide'}`} onClick={() => setIsNavbarCTAVisible(false)}>
-                  <i className="bi bi-x-lg"></i>
-               </div>
-            </div>
-         </div >
-      </>
-   )
-}
+  useEffect(() => {
+    if (isNavbarCTAVisible) {
+      document.querySelector('.navbar-cta').classList.add('visible');
+    } else {
+      document.querySelector('.navbar-cta').classList.remove('visible');
+    }
+  }, [isNavbarCTAVisible]);
 
-export default NavbarCTA
+  return (
+    <>
+      <div className={`navbar-cta ${isNavbarCTAVisible ? 'visible' : 'hide'}`}>
+        <div className="container">
+          <div className="text">
+            Just a friendly reminder. Your brand will die without a good website. ☠️
+          </div>
+          <Link to="/contact" className="link" onClick={() => setIsNavbarCTAVisible(false)}>
+            <span className="link-text">Help us fix</span>
+            <span className="link-icon">
+              <i className="bi bi-check-circle-fill"></i>
+            </span>
+          </Link>
+          <div className="close" onClick={() => setIsNavbarCTAVisible(false)}>
+            <i className="bi bi-x-lg"></i>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavbarCTA;
